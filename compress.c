f65ca9d378acb9e0ff780bb1c18bcaa3eae3d075
mitcmurph <mitcmurph@gmail.com>
2021-05-11 06:04:06 -0700

    store index to node not pointer

--- compress.c
+++ compress.c
@@ -65,7 +65,7 @@ void count_bytes(char *filename, hashmap_t *map)
 void func2(int pos, void *value)
 {
     node_t *c = (node_t *)(value);
-    printf("%p [%d]\t%02x\t%d\n", value, pos, c->key, c->value);
+    printf("%p [%d]\t%c\t%d\n", value, pos, c->key, c->value);
 }
 
 int comp(const void *a, const void *b)
@@ -110,7 +110,7 @@ void print_tree_rec(node_t *n, int i)
         return;
     }
     print_tab(i);
-    printf("%p [%d]\n", n, n->value);
+    printf("%p [%d] %02x\n", n, n->value, n->key);
     print_tab(i);
     printf("\t%p\t%p\n", n->left, n->right);
     print_tree_rec(n->left, i + 1);
@@ -119,28 +119,13 @@ void print_tree_rec(node_t *n, int i)
 
 void tranverse(node_t *root)
 {
-    if (root == NULL)
-    {
-        return;
-    }
     vector_t stack;
-    init_vector(&stack, 10, sizeof(node_t*));
+    init_vector(&stack, 10, sizeof(node_t *));
+    printf("%p\n", root);
+    vector_push_back(&stack, (void *)&root);
 
-    vector_push_back(&stack, &root);
-    while (stack.size > 0)
-    {
-        node_t *node = (node_t*)vector_pop(&stack);
-        if (node == NULL) break;
-        printf("%d %p %p\n", node->key, node->right, node->left);
-        if (node->right != NULL)
-        {
-            vector_push_back(&stack, (void*)node->right);
-        }
-        if (node->left != NULL)
-        {
-            vector_push_back(&stack, (void*)node->left);
-        }
-    }
+    node_t *jp = *(node_t **)vector_get(stack, 0);
+    printf("%p\n", jp);
 }
 
 /* PUBLIC */
@@ -160,30 +145,41 @@ void compress()
     while (v.size > 1)
     {
         int curr = v.size - 1;
-        void *ap = vector_get(v, curr);
-        void *bp = vector_get(v, curr - 1);
+        node_t *ap = vector_get(v, curr);
+        node_t *bp = vector_get(v, curr - 1);
 
-        ap = vector_push_back(&nodes, ap);
-        bp = vector_push_back(&nodes, bp);
-
-        vector_remove(&v, curr);
-        vector_remove(&v, curr - 1);
+        vector_push_back(&nodes, ap);
+        int ai = nodes.size;
+        vector_push_back(&nodes, bp);
+        int bi = nodes.size;
+        
+        ap = vector_get(v, ai);
+        bp = vector_get(v, bi);
+        printf("%c\t%d\n", ap->key, ap->value);
+        printf("%c\t%d\n", bp->key, bp->value);
 
         node_t new;
         new.in_use = 1;
         new.key = 0;
-        new.value = ((node_t *)ap)->value + ((node_t *)bp)->value;
+        new.value = ap->value + bp->value;
         new.left = ap;
         new.right = bp;
+
+        printf("new value: %d\n", new.value);
+
+        vector_remove(&v, curr);
+        vector_remove(&v, curr - 1);
 
         vector_push_back(&v, &new);
         vector_sort(v, comp2);
     }
+    print_vector(nodes, func2);
 
+/*
     node_t *root = (node_t *)vector_get(v, 0);
-    /*print_tree_rec(root, 0);*/
-    printf("%p\n", root->right);
-    tranverse(root);
+    print_tree_rec(root, 0);
+    printf("%d\n", root->value);
+    tranverse(root);*/
 
     free_hashmap(&map);
     free_vector(v);
