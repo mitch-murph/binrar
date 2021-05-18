d9f6a4309a74418173985018e083aa016cb20fe8
mitcmurph <mitcmurph@gmail.com>
2021-05-17 22:27:48 -0700

    Fix fputc

--- cipher.c
+++ cipher.c
@@ -8,7 +8,7 @@ int XORcipher(FILE *in_file, FILE *out_file, char *key)
     int i;
     int keyLen = strlen(key);
     char buffer;
-    while ((buffer = fget(in_file)) != EOF)
+    while ((buffer = fgetc(in_file)) != EOF)
     {
         buffer = buffer ^ key[i++ % keyLen];
         fputc(buffer, out_file);
