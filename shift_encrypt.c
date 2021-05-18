85577c82922d52424088bc1ca179a6d52f4dc66a
mitcmurph <mitcmurph@gmail.com>
2021-05-17 22:36:16 -0700

    Put constant in #define

--- shift_encrypt.c
+++ shift_encrypt.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "shift_encrypt.h"
 
+#define SHIFT_AMOUNT 69
+
 void shift_encrypt(FILE *fd1, FILE *fd2)
 {
     int shiftCode;
     while ((shiftCode = fgetc(fd1)) != EOF)
     {
-        shiftCode = shiftCode + 69;
+        shiftCode = shiftCode + SHIFT_AMOUNT;
         fputc(shiftCode, fd2);
     }
 }
@@ -17,7 +19,7 @@ void shift_decrypt(FILE *fd1, FILE *fd2)
     while ((shiftCode = fgetc(fd1)) != EOF)
     {
 
-        shiftCode = shiftCode - 69;
+        shiftCode = shiftCode - SHIFT_AMOUNT;
         fputc(shiftCode, fd2);
     }
 }
