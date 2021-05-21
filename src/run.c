a995fb105c2b09f9bef20e20f047ee3b20cc4fcb
mitcmurph <mitcmurph@gmail.com>
2021-05-21 04:08:43 -0700

    Refactor and pass in FILE pointers

--- src/run.c
+++ src/run.c
@@ -1,93 +1,51 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
 
-char *compress(char *str)
+int compress(FILE *in_fp, FILE *out_fp)
 {
-    int a;
+    int a = 0;
     int b = 0;
     int c = 1;
-    int size_str = strlen(str);
-    char *compress_str = malloc(size_str + 1);
-    if (size_str < 2)
-    {
-        return str;
-    }
 
-    for (a = 0; a < size_str; a++)
+    int prev;
+    int temp;
+    int buffer;
+    while ((buffer = fgetc(in_fp)) != EOF)
     {
         if (a == 0)
         {
-            compress_str[b] = str[a];
+            temp = buffer;
         }
         else
         {
-            if (str[a] == str[a - 1])
+            prev = temp;
+            temp = buffer;
+            if (prev == temp)
             {
-                compress_str[b] = str[a];
-                if (c >= 9 && c < 99)
-                {
-                    c++;
-                    compress_str[b + 1] = (c / 10) + 48;
-                    compress_str[b + 2] = (c % 10) + 48;
-                }
-                else if (c >= 99)
-                {
-                    c++;
-                    compress_str[b + 1] = (c / 100) + 48;
-                    compress_str[b + 2] = ((c / 10) % 10) + 48;
-                    compress_str[b + 3] = (c % 10) + 48;
-                }
-                else
-                {
-                    c++;
-                    compress_str[b + 1] = c + 48;
-                }
+                c++;
             }
             else
             {
-                if (c >= 10 && c < 100)
-                {
-                    b = b + 3;
-                    c = 1;
-                    compress_str[b] = str[a];
-                }
-                else if (c >= 100)
-                {
-                    b = b + 4;
-                    c = 1;
-                    compress_str[b] = str[a];
-                }
-                else if (c > 1 && c <= 9)
-                {
-                    b = b + 2;
-                    c = 1;
-                    compress_str[b] = str[a];
-                }
-                else if (c == 1)
-                {
-                    b++;
-                    compress_str[b] = str[a];
-                }
+                printf("%d%c", c, prev);
+                c = 0;
             }
         }
+        a++;
     }
-    return compress_str;
+    printf("%d%c", c, prev);
+    return a;
 }
 
 int main()
 {
-    printf("type in sample string \n");
 
-    char *res;
-    char *str;
-    str = (char *)malloc(10240 * sizeof(char));
-    scanf("\n%[^\n]", str);
-    printf("Decompressed state: \n%s\n", str);
+    FILE *in_fp = fopen("in", "rb");
+    FILE *out_fp = fopen("out", "wb");
 
-    res = compress(str);
-    printf("Compressed state: \n%s\n", res);
+    compress(in_fp, out_fp);
+
+    fclose(in_fp);
+    fclose(out_fp);
     return 0;
 }
