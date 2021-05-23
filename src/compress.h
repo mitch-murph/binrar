5bccb01f337585761ac24342ec3f2875117218e7
mitcmurph <mitcmurph@gmail.com>
2021-05-23 09:24:36 -0700

    Move run length compression to compress file

--- src/compress.h
+++ src/compress.h
@@ -8,5 +8,7 @@
 *****************************************************************************/
 void huffmanCompress(FILE *in_fp, FILE *out_fp);
 void huffmanDecompress(FILE *in_fp, FILE *out_fp);
+void runLengthCompress(FILE *in_fp, FILE *out_fp);
+void runLengthDecompress(FILE *in_fp, FILE *out_fp);
 
 #endif
