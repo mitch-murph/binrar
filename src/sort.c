275985982a9cef5a2f4cfc8307800d1a9e5ed731
pmanav <82566221+pmanav@users.noreply.github.com>
2021-05-28 21:48:29 +1000

    Update sort.c

--- src/sort.c
+++ src/sort.c
@@ -9,7 +9,8 @@ void sort(vector_t vector, int (*compare)(const void *a, const void *b))
     {
         for (i = 0; i < vector.size - j - 1; i++)
         {
-            /*compares the elements to see if further sorting is required. If it does it will run through it again and if not it terminates*/
+            /*compares the elements to see if further sorting is required. 
+            If it does it will run through it again and if not it terminates*/
             if (compare(vectorGet(vector, i), vectorGet(vector, i + 1)) > 0)
             {
                 vectorSwap(vector, i, i + 1);
