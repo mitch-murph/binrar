6b2f59dcf8f02791c2df5b175a294c36b44c6c6f
mitcmurph <mitcmurph@gmail.com>
2021-05-28 05:31:44 -0700

    Put comments in same format

--- src/encrypt.c
+++ src/encrypt.c
@@ -1,4 +1,3 @@
-  
 /*****************************************************************************
  * 48430 Fundamentals of C Programming - Assignment 3
  * Encrypt
@@ -6,29 +5,23 @@
     1. Shift encryption
     2. XOR Cipher
 *****************************************************************************/
-
-#include <stdio.h> /* fputc*/
+#include <stdio.h>  /* fputc*/
 #include <string.h> /* strlen*/
-#include "encrypt.h" 
-
+#include "encrypt.h"
 
 #define SHIFT_AMOUNT 69
 
-
-/******************************************************************************
-* This function reads content from file 1 to encrypt/decrypts and write it
-* in file 2. 
-
- *   Input:
- *   read content in file 1
-
- *   Output:
- *   write content in file 2.
- 
-******************************************************************************/
-
+/*****************************************************************************
+ * This function performs the actual shift encryption/decryption process.
+ * Input:
+ *   fd1 - The input file pointer.
+ *   fd2 - The output file pointer
+ * Post:
+ *   The output file will contain the encrypted/decryption text.
+*****************************************************************************/
 void shift_encrypt_bytes(FILE *fd1, FILE *fd2, int amount)
 {
+    /* Loop over each byte and add the shift amount. */
     int shiftCode;
     while ((shiftCode = fgetc(fd1)) != EOF)
     {
@@ -41,63 +34,65 @@ void shift_encrypt_bytes(FILE *fd1, FILE *fd2, int amount)
  * This function encyrpts the file of diffrent types by shifting the ASCII 
  * number of original bytes by the defined shift amount and then the shifted
  * ASCII value is substituted as its corresponding characters. 
- 
- * Example: 
-            input character : A
-            ASCII character of A: 65
-            Adding shift_amount (69) to ASCII character of A: 134
-            134 ASCII code to corresponding character : å
- 
-        
  * Input:
- *   input text: Hello everyone
-
- * Output:
- *   output text: Mjqqt%j{jw~tsj
+ *   fd1 - The input file pointer.
+ *   fd2 - The output file pointer
+ * Post:
+ *   The output file will contain the encrypted text.
+ * Example: 
+ *   input character : A
+ *   ASCII character of A: 65
+ *   Adding shift_amount (69) to ASCII character of A: 134
+ *   134 ASCII code to corresponding character : å
 *****************************************************************************/
-
 void shift_encrypt(FILE *fd1, FILE *fd2)
 {
     /* encrypt the file by substituting with ASCII code by shifting 
-       forward by the defined amount */ 
+       forward by the defined amount */
     shift_encrypt_bytes(fd1, fd2, SHIFT_AMOUNT);
 }
 
 /*****************************************************************************
  * This function dencyrpts the file the ASCII characters to the original
  * file. 
-  
-    * Example: 
-            input character : å
-            ASCII character of å : 134
-            Subtracting shift_amount (69) to ASCII character of å: 65
-            134 ASCII code to corresponding character : A
-
  * Input:
- *   input text: Mjqqt%j{jw~tsj
-
- * Output:
- *   output text: Hello everyone
+ *   fd1 - The input file pointer.
+ *   fd2 - The output file pointer
+ * Post:
+ *   The output file will contain the encrypted text.
+ * Example: 
+ *   input character : å
+ *   ASCII character of å : 134
+ *   Subtracting shift_amount (69) to ASCII character of å: 65
+ *   134 ASCII code to corresponding character : A
 *****************************************************************************/
-
 void shift_decrypt(FILE *fd1, FILE *fd2)
 {
     /* decrypt the file to shifted backward by the defined amount 
         to get original text*/
-
     shift_encrypt_bytes(fd1, fd2, -SHIFT_AMOUNT);
 }
 
 /*****************************************************************************
- * This function encrypts/decrypt the file using XOR cipher. 
-
+ * This function encrypts the file using an XOR cipher, an additive cipher 
+ * using a supplied key. This function is also used to decrypt the cipher.
+ * Input:
+ *   in_file - The input file pointer.
+ *   out_file - The output file pointer
+ *   key - The password to encrypt with
+ * Post:
+ *   The output file will contain the encrypted text.
+ * Example: 
+ *   input string : The quick brown fox jumps over the lazy dog
+ *   input key : password
+ *   ouput string : $	SADSARSDy
 *****************************************************************************/
-
 void XOR_cipher(FILE *in_file, FILE *out_file, char *key)
 {
     int i = 0;
     int keyLen = strlen(key);
     int buffer;
+    /* Loop over each byte and XOR the corresponding bytes of the key. */
     while ((buffer = fgetc(in_file)) != EOF)
     {
         buffer = buffer ^ key[i++ % keyLen];
--- src/sort.c
+++ src/sort.c
@@ -1,16 +1,35 @@
+/*****************************************************************************
+ * 48430 Fundamentals of C Programming - Assignment 3
+ * Sort
+ * The implemenation of the sort function.
+*****************************************************************************/
+
 #include "vector.h"
 #include "sort.h"
-/*function to implement the sort*/
+
+/*****************************************************************************
+ * This function implement the bubble sort.
+ * Input:
+ *   vector - The vector to be sorted.
+ *   compare - The function to compare the values by.
+ * Post:
+ *   The index of the value in the vector.
+ *   If not found -1.
+*****************************************************************************/
 void sort(vector_t vector, int (*compare)(const void *a, const void *b))
 {
-    /*checks to see if the element needs to be moved to the adjacent position*/
+    /* Loop over all positions */
     int i, j;
     for (j = 0; j < vector.size - 1; j++)
     {
+        /* Comparing them to each other position past it. 
+           It will compares the elements to see if further 
+            sorting is required. If it does it will run 
+            through it again and if not it terminates */
         for (i = 0; i < vector.size - j - 1; i++)
         {
-            /*compares the elements to see if further sorting is required. 
-            If it does it will run through it again and if not it terminates*/
+            /*checks to see if the element needs to be 
+              moved to the adjacent position*/
             if (compare(vectorGet(vector, i), vectorGet(vector, i + 1)) > 0)
             {
                 vectorSwap(vector, i, i + 1);
